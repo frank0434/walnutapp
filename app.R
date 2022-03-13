e937adfa2c7858a1c6a0f04c3fd2e33eeff70e91
frank0434 <jian.liu@plantandfood.co.nz>
2022-03-13 21:35:41 +1300

    change ui to have weather period and input NIWA account name

--- app.R
+++ app.R
@@ -9,7 +9,7 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws a histogram ----------
 ui <- fluidPage(
 
     # Application title
@@ -18,11 +18,12 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            textInput(inputId = "cliflo_user", 
+                      label = "Cliflo account username"), 
+            dateRangeInput(inputId = "date_range", 
+                           label = "Climate period",
+                           start = "2021-09-01")
+            
         ),
 
         # Show a plot of the generated distribution
@@ -32,16 +33,14 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a histogram ----------
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+       
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
 }
 
